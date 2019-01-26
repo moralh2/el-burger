208229bb6c080f38adb47b2d311e6d4320b8ff9f
Hector M <moralh2@gmail.com>
2019-01-26 14:56:37 -0600

    add select-all in orm

---
+++ config/orm.js
@@ -0,0 +1,17 @@
+// Import the MySQL connection
+var connection = require("./connection")
+
+
+// Object Relational Mapper
+
+var orm = {
+    selectAll: function(tableName) {
+        var queryString = "SELECT * FROM ??";
+        connection.query(queryString, [tableName], function(error, result) {
+            if (error) throw error;
+            console.log(result);
+        });
+    }
+}
+
+module.exports = orm;
