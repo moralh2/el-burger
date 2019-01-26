b6b941b322045e165d7351032a3349ded69882b9
Hector M <moralh2@gmail.com>
2019-01-26 14:40:49 -0600

    add schema, seeds files

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers (name) VALUES ('Bacon Cheeseburger');
+INSERT INTO burgers (name) VALUES ('Cheeseburger');
+INSERT INTO burgers (name) VALUES ('Buffalo Burger');
+
+INSERT INTO burgers (name, devoured) VALUES ('California Burger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Tuna Burger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Mushroom Burger', true);
