6bf1dca568b932d7815068df4d7bd81c8c6bbc0c
Hector M <moralh2@gmail.com>
2019-01-28 23:21:27 -0600

    add route for devour method

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -7,7 +7,7 @@ var router = express.Router();
 // Import the model for burger
 var burger = require("../models/burger.js");
 
-// Create home route
+// Home route
 router.get("/", function (request, response) {
     burger.all(function (data) {
         var hbsObject = {
@@ -18,10 +18,23 @@ router.get("/", function (request, response) {
     });
 });
 
-router.post("/api/burgers", function(request, response) {
+// Post route for burgers -- only requires burgerName in request
+router.post("/api/burgers", function (request, response) {
     burger.insertNew(request.body.burgerName, function (result) {
         console.log(result)
         response.json({ id: result.insertId });
+    });
+});
+
+// Put route -- only to 1 column (devour), so only need ID
+router.put("/api/burgers/:id", function (request, response) {
+    burger.eat(request.params.id, function (result) {
+        // 404 for no rows changed
+        if (result.changedRows == 0) {
+            return response.status(404).end();
+        } else {
+            response.status(200).end();
+        }
     });
 });
 
