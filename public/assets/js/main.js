868e26acf978bb2ee8339237e69783097cacb366
Hector M <moralh2@gmail.com>
2019-01-29 00:25:36 -0600

    add jquery to create new burger

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,11 +1,29 @@
 $(function(){
+
     $(".devour").on("click", function(event) {
         var id = $(this).data("id");
-        
         $.ajax("/api/burgers/" + id, {type: "PUT"})
         .then( function() {
             console.log("And off you go to burger heaven");
+            // Reload to update list
             location.reload();
-        })
-    })
-})
+        });
+    });
+
+    $(".post-burger").on("submit", function(event) {
+        event.preventDefault();
+        var userInput = { burgerName: $("#burgerName").val().trim() }
+        console.log(userInput);
+
+        $.ajax("/api/burgers", {
+            type: "POST",
+            data: userInput
+        }).then(
+            function() {
+                console.log("A new burger approaches");
+                location.reload();
+            }
+        );
+    });
+
+});
